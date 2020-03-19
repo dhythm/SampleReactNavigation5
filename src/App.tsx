5518ca65b23629a8723ea77e616db1f2f62aad60
dhythm <Yuta.Okada.20@gmail.com>
2020-03-20 05:58:31 +0800

    feat: add deep linking

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,55 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React, { useEffect } from 'react';
+import { NavigationContainer, useLinking } from '@react-navigation/native';
+import React, { useEffect, useRef, useState } from 'react';
 import AuthProvider from './AuthProvider';
 import HomeNavigator from './Home';
 import NavigationService from './navigation-service';
+
+const AppWithDeepLinking: React.FunctionComponent = () => {
+  const ref = useRef();
+  const { getInitialState } = useLinking(ref, {
+    prefixes: ['srn5://'],
+    config: {
+      Details: 'details',
+      // HomeNavigator: {
+      //   screens: {
+      //     Details: 'details',
+      //   },
+      // },
+    },
+  });
+
+  const [isReady, setIsReady] = useState(false);
+  const [initialState, setInitialState] = useState();
+
+  useEffect(() => {
+    Promise.race([
+      getInitialState(),
+      new Promise((resolve) => setTimeout(resolve, 150)),
+    ])
+      .catch((e) => {
+        console.error(e);
+      })
+      .then((state) => {
+        if (state !== undefined) {
+          setInitialState(state);
+        }
+
+        setIsReady(true);
+      });
+  }, [getInitialState]);
+
+  if (!isReady) {
+    return null;
+  }
+
+  return (
+    <AuthProvider>
+      <NavigationContainer initialState={initialState} ref={ref}>
+        <HomeNavigator />
+      </NavigationContainer>
+    </AuthProvider>
+  );
+};
 
 const App: React.FunctionComponent = () => {
   useEffect(() => {
@@ -24,4 +71,5 @@ const App: React.FunctionComponent = () => {
   );
 };
 
-export default App;
+// export default App;
+export default AppWithDeepLinking;
